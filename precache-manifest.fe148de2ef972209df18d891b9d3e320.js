eb83c8a4ac6bfc36b2d5f04e9bb0aa4c40cd5afb
Nao <naufali@sola-interactive.com>
2020-02-10 14:43:41 +0700

    Updates

---
+++ precache-manifest.fe148de2ef972209df18d891b9d3e320.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "746d629a6d663a204d3f62b5c3a1205a",
+    "url": "/rnaufali00/pokemons/index.html"
+  },
+  {
+    "revision": "b45fa03efb566ca0c85b",
+    "url": "/rnaufali00/pokemons/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "8d4046dfddaa2441ab66",
+    "url": "/rnaufali00/pokemons/static/css/main.a8e95f69.chunk.css"
+  },
+  {
+    "revision": "b45fa03efb566ca0c85b",
+    "url": "/rnaufali00/pokemons/static/js/2.0f356e34.chunk.js"
+  },
+  {
+    "revision": "12570cd1c8f03b5daf36b1f621313989",
+    "url": "/rnaufali00/pokemons/static/js/2.0f356e34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d4046dfddaa2441ab66",
+    "url": "/rnaufali00/pokemons/static/js/main.328a7382.chunk.js"
+  },
+  {
+    "revision": "417746637087d952477e",
+    "url": "/rnaufali00/pokemons/static/js/runtime-main.90379c97.js"
+  }
+]);
